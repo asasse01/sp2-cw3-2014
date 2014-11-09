54c18699250097e72b8c7184622012de3eddba48
jackscotti <jacopojackscotti@gmail.com>
2014-11-09 14:13:02 +0000

    Add comments

--- Coursework3/src/Customer.java
+++ Coursework3/src/Customer.java
@@ -11,12 +11,14 @@ public class Customer {
 		return currentFloor;
 	}
 	public void setCurrentFloor(int currentFloor) {
+		// add random current floor from 0 to number of floors - 1 
 		this.currentFloor = currentFloor;
 	}
 	public int getDestinationFloor() {
 		return destinationFloor;
 	}
 	public void setDestinationFloor(int destinationFloor) {
+		// add random destination floor from 0 to number of floors - 1 
 		this.destinationFloor = destinationFloor;
 	}
 	public int getId() {
