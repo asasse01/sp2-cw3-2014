505860fc9fb9927823ad358fd182cf411e49c1fd
jackscotti <jacopojackscotti@gmail.com>
2014-11-09 13:05:25 +0000

    Set up Customer class

--- Coursework3/src/BuildingTest.java
+++ Coursework3/src/BuildingTest.java
@@ -11,10 +11,20 @@ public class BuildingTest {
 		Building b = new Building();
 		b.setNumberOfFloors(100);
     	assertEquals(100, b.getNumberOfFloors());
-    	
+ 
     	b.setNumberOfFloors();
     	assertEquals(10, b.getNumberOfFloors());
 	}
+	
+//	@Test
+//	public void setNumberOfCustomers(){
+//		Customer c = new Customer();
+//		c.setNumberOfCustomers(100);
+//    	assertEquals(100, c.getNumberOfCustomers());
+// 
+//    	c.setNumberOfCustomers();
+//    	assertEquals(10, c.getNumberOfCustomers());
+//	}
 }
 
 
--- Coursework3/src/Customer.java
+++ Coursework3/src/Customer.java
@@ -1,4 +1,41 @@
 
 public class Customer {
-
+	
+	private int currentFloor;
+	private int destinationFloor;
+	private int Id;
+	private boolean inElevator;
+	private boolean finish;
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	public void setCurrentFloor(int currentFloor) {
+		this.currentFloor = currentFloor;
+	}
+	public int getDestinationFloor() {
+		return destinationFloor;
+	}
+	public void setDestinationFloor(int destinationFloor) {
+		this.destinationFloor = destinationFloor;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public boolean isInElevator() {
+		return inElevator;
+	}
+	public void setInElevator(boolean inElevator) {
+		this.inElevator = inElevator;
+	}
+	public boolean isFinish() {
+		return finish;
+	}
+	public void setFinish(boolean finish) {
+		this.finish = finish;
+	}
+	
 }
