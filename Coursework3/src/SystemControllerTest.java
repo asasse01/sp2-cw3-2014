f7991f8d45b0e56e0b7bf256fef5271898d592fe
= <sassela@live.com>
2014-11-16 13:38:02 +0000

    Add defaultStrategy() test

--- Coursework3/src/SystemControllerTest.java
+++ Coursework3/src/SystemControllerTest.java
@@ -19,4 +19,26 @@ public class SystemControllerTest {
     	assertEquals(9, SystemController.getBuilding().getCustomerList().get(9).getId());
 	}
 
+	@Test
+	public void defaultStrategy(){
+		Elevator e1 = new Elevator(); // should be new Building?
+
+		Customer c1 = new Customer();
+		Customer c2 = new Customer();
+		Customer c3 = new Customer();
+
+		c1.setDestinationFloor(5);
+		c2.setDestinationFloor(5);
+		c3.setDestinationFloor(8);
+
+		e1.customerJoins(c1);
+		e1.customerJoins(c2);
+		e1.customerJoins(c3);
+
+		// check starting number is correct
+		assertEquals(3, e1.getNumberOfCustomers());
+		
+		e1.defaultStrategy();
+		assertEquals(0, e1.getNumberOfCustomers());
+	}
 }
