6e2897d96b694a5506b6bf1c9b74a828639d303d
= <sassela@live.com>
2014-11-16 21:21:48 +0000

    Remove infinite loop

--- Coursework3/src/SystemController.java
+++ Coursework3/src/SystemController.java
@@ -33,29 +33,27 @@ public class SystemController {
 		int count = 0;
 		Building building = getBuilding();
 		boolean simulationFinished = false;
+	
+		while (!simulationFinished) {
+			do {
+			// customer gets out if at destination floor
+			building.getElevator().unload();
+			// customer gets in if at same floor as elevator
+			building.load();
+
+			count++;
+			building.getElevator().move();
+			} while ((building.getElevator().getCurrentFloor() > 0) && (building.getElevator().getCurrentFloor() < building.getNumberOfFloors()));			
 		
-		do  {
+			building.getElevator().switchDirection();
+			
 			for (Customer customer : building.getCustomerList()) {
 	            // TODO method to check all customers are on destination floor
 	            if (!(customer.isFinished())) {
 	                simulationFinished = false;
 	            } else simulationFinished = true;
 	        }
-	
-			if (!simulationFinished) {
-				do {
-				// customer gets out if at destination floor
-				building.getElevator().unload();
-				// customer gets in if at same floor as elevator
-				building.load();
-
-				count++;
-				building.getElevator().move();
-				} while ((building.getElevator().getCurrentFloor() > 0) && (building.getElevator().getCurrentFloor() < building.getNumberOfFloors()));			
-			
-				building.getElevator().switchDirection();
-								
-			} else return count;
-		} while(true);
+							
+		} return count;
 	}
 }
