7ac4f90a04982e911b20a3878bdaa47b04d4e896
= <sassela@live.com>
2014-11-25 06:49:37 +0000

    Separate generateCustomer Test. Add clearSystemData method

--- Coursework3/src/SystemController.java
+++ Coursework3/src/SystemController.java
@@ -3,67 +3,67 @@ public class SystemController {
 	final static int FLOORS = 10;
 	static boolean simulationFinished = false;
 	private static int efficiencyCounter;
-	
+
 	public static void main(String[] args) {
-		
-		// TODO:  ask User the number of Customers
+
+		// TODO: ask User the number of Customers
 		generateCustomers();
 	}
-	
-	public static int defaultNumberOfCustomers(){
+
+	public static int defaultNumberOfCustomers() {
 		return 10;
 	}
-	
-	public static boolean getSimulationStatus(){
+
+	public static boolean getSimulationStatus() {
 		return simulationFinished;
 	}
 
-	public static void setSimulationStatus(boolean status){
+	public static void setSimulationStatus(boolean status) {
 		SystemController.simulationFinished = status;
 	}
-	
-	public static void generateCustomers(){ 
+
+	public static void generateCustomers() {
 		// setting default number of customers
-		while (building.getCustomerList().size() != defaultNumberOfCustomers()){ // to do recursively
+		while (building.getCustomerList().size() != defaultNumberOfCustomers()) { // to
+																					// do
+																					// recursively
 			Customer customer = new Customer(FLOORS);
 			building.addCustomer(customer);
 		}
 	}
-	
-	public static void generateCustomers(int number){ 
+
+	public static void generateCustomers(int number) {
 		// setting user specified number of customers
-		while (building.getCustomerList().size() != number){ // to do recursively
+		while (building.getCustomerList().size() != number) { // to do
+																// recursively
 			Customer customer = new Customer(FLOORS);
 			building.addCustomer(customer);
 		}
 	}
-	
-	public static Building getBuilding(){
+
+	public static Building getBuilding() {
 		return building;
 	}
-	
 
-	
 	public static void defaultStrategy() {
-		
+
 		setEfficiencyCounter(0);
-		
+
 		Building building = getBuilding();
-		
-	
+
 		while (!simulationFinished) {
 			do {
-			// customer gets out if at destination floor
-			building.getElevator().unload();
-			// customer gets in if at same floor as elevator
-			building.load();
+				// customer gets out if at destination floor
+				building.getElevator().unload();
+				// customer gets in if at same floor as elevator
+				building.load();
 
-			setEfficiencyCounter(getEfficiencyCounter() + 1); // to refactor
-			building.getElevator().move();
-			} while (withinFloorLimits());			
-		
+				setEfficiencyCounter(getEfficiencyCounter() + 1); // to refactor
+				building.getElevator().move();
+			} while (withinFloorLimits());
+
 			building.getElevator().switchDirection();
-			updateSimulationStatus();				
+			updateSimulationStatus();
 		}
 	}
 
@@ -74,53 +74,63 @@ public class SystemController {
 	static void setEfficiencyCounter(int efficiencyCounter) {
 		SystemController.efficiencyCounter = efficiencyCounter;
 	}
-	
+
 	public static boolean withinFloorLimits() {
-		if ((building.getElevator().getCurrentFloor() > 0) 
+		if ((building.getElevator().getCurrentFloor() > 0)
 				&& (building.getElevator().getCurrentFloor() < building.getNumberOfFloors())) {
 			return true;
-		}
-		else return false;
-		
+		} else
+			return false;
+
 	}
-	
+
 	public static void updateSimulationStatus() {
 		for (Customer customer : building.getCustomerList()) {
-            if (customer.isFinished()) {
-                setSimulationStatus(true);
-            } else setSimulationStatus(false);
-        }
+			if (customer.isFinished()) {
+				setSimulationStatus(true);
+			} else
+				setSimulationStatus(false);
+		}
 	}
-	
+
 	public static void alternativeStrategy() {
-		
+
 		setEfficiencyCounter(0);
-		
+
 		Building building = getBuilding();
-		
-	
+
 		while (!simulationFinished) {
 			do {
-			// customer gets out if at destination floor
-			building.getElevator().unload();
-			
-			for (Customer customer : building.getCustomerList()) {
-				// refactor
-				if (((customer.getDestinationFloor() - customer.getCurrentFloor() > 0) &&
-						building.getElevator().getDirection() == 1) || 
-						((customer.getDestinationFloor() - customer.getCurrentFloor() < 0) &&
-								building.getElevator().getDirection() == -1)) {
-					// customer gets in if at same floor as elevator
-					building.load();
-				}
-			}
+				// customer gets out if at destination floor
+				building.getElevator().unload();
 
-			setEfficiencyCounter(getEfficiencyCounter() + 1); // to refactor
-			building.getElevator().move();
-			} while (withinFloorLimits());			
-		
+				for (Customer customer : building.getCustomerList()) {
+					// refactor
+					if (((customer.getDestinationFloor()
+							- customer.getCurrentFloor() > 0) && building
+							.getElevator().getDirection() == 1)
+							|| ((customer.getDestinationFloor()
+									- customer.getCurrentFloor() < 0) && building
+									.getElevator().getDirection() == -1)) {
+						// customer gets in if at same floor as elevator
+						building.load();
+					}
+				}
+
+				setEfficiencyCounter(getEfficiencyCounter() + 1); // to refactor
+				building.getElevator().move();
+			} while (withinFloorLimits());
+
 			building.getElevator().switchDirection();
-			updateSimulationStatus();				
+			updateSimulationStatus();
 		}
+	}
+	
+	public static void clearSystemData() {
+		
+		setSimulationStatus(false);
+		building.getCustomerList().clear();
+		setEfficiencyCounter(0);
+		
 	}
 }
--- Coursework3/src/SystemControllerTest.java
+++ Coursework3/src/SystemControllerTest.java
@@ -6,38 +6,52 @@ public class SystemControllerTest {
 	
 	final int FLOORS = 10;
 	
-//	@Test
-//	public void defaultStrategy() {
-//		
-//		SystemController.generateCustomers();
-//		Building b1 = SystemController.getBuilding();
-//		Elevator e1 = b1.getElevator();
-//		
-//		// test status
-//		assertEquals(false, SystemController.getSimulationStatus());
-//		// test number of customers
-//		assertEquals(10, SystemController.getBuilding().getCustomerList().size());
+	@Test
+	public void generateCustomers() {
+		
+		SystemController.generateCustomers();
+		// test number of customers
+		assertEquals(10, SystemController.getBuilding().getCustomerList().size());
+		// note: decide whether we should have single instance of SystemController that is cleared after use or new instances
 //		// tests if IDs are assigned correctly
 //    	assertEquals(1, SystemController.getBuilding().getCustomerList().get(0).getId());
 //    	assertEquals(4, SystemController.getBuilding().getCustomerList().get(3).getId());
 //    	assertEquals(8, SystemController.getBuilding().getCustomerList().get(7).getId());
 //    	assertEquals(10, SystemController.getBuilding().getCustomerList().get(9).getId());
-//		
-//		e1.setDirection(1); // refactor
-//
-//		SystemController.defaultStrategy();
-//		assertEquals(true, SystemController.getSimulationStatus());
-//
-//		System.out.println("Count: " + SystemController.getEfficiencyCounter());
-//
-//	}
+    	
+    	SystemController.clearSystemData();
+
+	}
 	
 	@Test
-	public void alternativeStrategy() {
-
+	public void defaultStrategy() {
+		
 		SystemController.generateCustomers();
 		Building b1 = SystemController.getBuilding();
 		Elevator e1 = b1.getElevator();
+		
+		// test status
+		assertEquals(false, SystemController.getSimulationStatus());
+		
+		e1.setDirection(1); // refactor
+
+		SystemController.defaultStrategy();
+		assertEquals(true, SystemController.getSimulationStatus());
+
+		System.out.println("Count: " + SystemController.getEfficiencyCounter());
+
+		SystemController.clearSystemData();
+	}
+	
+	@Test
+	public void alternativeStrategy() {
+		
+		SystemController.generateCustomers();
+		Building b1 = SystemController.getBuilding();
+		Elevator e1 = b1.getElevator();
+		
+		// test status
+		assertEquals(false, SystemController.getSimulationStatus());
 
 		e1.setDirection(1); // refactor
 
@@ -45,6 +59,8 @@ public class SystemControllerTest {
 		assertEquals(true, SystemController.getSimulationStatus());
 
 		System.out.println("Count: " + SystemController.getEfficiencyCounter());
+		
+		SystemController.clearSystemData();
 
 	}
 }
